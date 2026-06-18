eedc7f27cbfb1cdc1fc3a6a1c8b0366f6ccc562f
Angie Arias <angiearias32@gmail.com>
2026-06-17 21:29:51 -0600

    Paredes y limites internos

--- experiments/movimiento.c
+++ experiments/movimiento.c
@@ -12,21 +12,35 @@ int main () { // funcion principal
     while (1) { // aqui se crea un ciclo infinito
         clear(); // borra la pantalla para que no quede las posiciones viejas del personaje
         
+        for (int i = 0; i < 21; i++) {
+        
+            mvprintw(0, i, "#"); // dibuja la pared superior
+            mvprintw(20, i, "#"); // dibuja la pared inferior
+            
+        }
+        
+        for (int i = 0; i < 21; i++) {
+        
+            mvprintw(i, 0, "#"); // dibuja la pared izquierda
+            mvprintw(i, 20, "#"); // dibuja la pared derecha
+            
+        }
+        
         mvprintw(y, x, "@"); // aqui se mueve el cursor a una posicion y escribe algo
         
         refresh(); // hace visible lo que acabamos de dibujar
         
         tecla = getch(); // es donde se va a guardar la tecla arriba, abajo, izquierda, derecha
-        if (tecla == KEY_UP && y > 0)
+        if (tecla == KEY_UP && y > 1)
             y--;
             
-        else if (tecla == KEY_DOWN && y < 20)
+        else if (tecla == KEY_DOWN && y < 19)
             y++;
             
-        else if (tecla == KEY_LEFT && x > 0)
+        else if (tecla == KEY_LEFT && x > 1)
             x--;
             
-        else if (tecla == KEY_RIGHT && x < 20)
+        else if (tecla == KEY_RIGHT && x < 19)
             x++;
             
         else if (tecla == 'q')
